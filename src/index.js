4d8e7231fac2344168a49420e3f3bba83edb0f19
Adiel Amzaleg <idealamz@gmail.com>
2018-03-02 00:16:28 +0200

    adding comment - enable functions as dictionary value

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ export default (dictionary = {}) => (key, variables = {}) => {
     switch (typeof dictionary[key]) {
         case 'string':
             return template(dictionary[key])(variables).trim()
+        case 'function':
+            // TODO: evaluate function and recursively call the translate again with the returned value
+            break
         default:
             console.warn(`${key} doesn't exist`)
             return `!_MISSING_TRANSLATION_${key}_!`
